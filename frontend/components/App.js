6b1246f0fb67e20b2d6ddd9990307f69b48a2b92
Damon Allen <damon.nicholas.allen@live.com>
2023-08-29 20:41:59 -0400

    axios.get working

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import axios from 'axios'
+import Form from './Form'
+import TodoList from './TodoList'
+
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:9000/api/todos')
+    .then(res => this.setState({ todos: res.data.data }))
+    .catch(err => console.error(err));
+  }
+
+  onSubmit = name => {
+    axios.post('');
+  }
+
   render() {
-    return null
+    console.log(this.state.todos);
+    return (
+      <div>
+      <h2>Todos:</h2>
+      <TodoList todos={this.state.todos} />
+      </div>
+    )
   }
 }
