df319ed612836be3d3a453bd345740676d9dec01
Damon Allen <damon.nicholas.allen@live.com>
2023-08-29 21:23:00 -0400

    renders Todos

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+  }
   render() {
-    return null
+    return (
+      <div>
+        {this.props.todos.map((item) => {
+          return (<p key={item.id}>{item.name}</p>)
+        })}
+      </div>
+    )
   }
 }
